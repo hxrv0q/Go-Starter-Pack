dd3415531ad6b985678bb7ba786f35c705b02a41
seshotake <seshotake@gmail.com>
2023-07-02 12:25:55 +0300

    Part 1. Variables

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	var smsSendingLimit int
+	var costPerSMS float64
+	var hasPermission bool
+	var username string
+
+	fmt.Printf(
+		"%v %f %v %q\n",
+		smsSendingLimit,
+		costPerSMS,
+		hasPermission,
+		username,
+	)
 }
